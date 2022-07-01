3343603e905b9beaf2ce8fe34d09cecef147d9ee
Frances Lu <n86903s@gmail.com>
2022-07-01 14:20:28 +0800

    Updates

---
+++ precache-manifest.5d0278c3931d7e9d180cc0a1cfa05b8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd8c2380193de32257b7",
+    "url": "/remote-assignments/static/css/main.3cd9ba9d.chunk.css"
+  },
+  {
+    "revision": "bd8c2380193de32257b7",
+    "url": "/remote-assignments/static/js/main.bd8c2380.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/remote-assignments/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "5dafc65634842c2e35a3",
+    "url": "/remote-assignments/static/js/2.5dafc656.chunk.js"
+  },
+  {
+    "revision": "9366ddba3c40ce6a31a9",
+    "url": "/remote-assignments/static/js/runtime~main.9366ddba.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/remote-assignments/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "24e540879d67e8c94005974c230faed7",
+    "url": "/remote-assignments/index.html"
+  }
+];
